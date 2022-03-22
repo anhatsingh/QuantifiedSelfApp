7edd4692a2ad57d1b63d8dfd184b7840456d8922
Anhat Singh <anhatsingh2001@gmail.com>
2022-03-22 17:36:59 +0530

    final changes

--- application/controllers/default.py
+++ application/controllers/default.py
@@ -12,15 +12,19 @@ from datetime import datetime
 @app.route('/')
 @login_required
 def home_page():
-    # this will contain info about trackers of this logged in user
-    trackers = []
-    # get all trackers made by this user
-    for i in Tracker.query.filter_by(user_id=flask_login.current_user.id).all():
-        # get the last updated value of this tracker
-        updated_at = Tracker_log.query.filter_by(tracker_id=i.id).order_by(Tracker_log.timestamp.desc()).all()
-        updated_at = updated_at[0].timestamp if updated_at else "Never"
-        # add the info gathered to the list of trackers above.
-        trackers.append({'id': i.id, 'name': i.name, 'description': i.description, 'updated_at': updated_at})
-    return render_template('home.html', title='Home Page', trackers = trackers)
+    try:
+        # this will contain info about trackers of this logged in user
+        trackers = []
+        # get all trackers made by this user
+        for i in Tracker.query.filter_by(user_id=flask_login.current_user.id).all():
+            # get the last updated value of this tracker
+            updated_at = Tracker_log.query.filter_by(tracker_id=i.id).order_by(Tracker_log.timestamp.desc()).all()
+            updated_at = updated_at[0].timestamp if updated_at else "Never"
+            # add the info gathered to the list of trackers above.
+            trackers.append({'id': i.id, 'name': i.name, 'description': i.description, 'updated_at': updated_at})
+        return render_template('home.html', title='Home Page', trackers = trackers)
+    except:
+        app.logger.exception("error occurred")
+        abort(500)
 
 # ========================================================================================================================
--- application/controllers/log.py
+++ application/controllers/log.py
@@ -8,6 +8,7 @@ from flask_security import login_required
 import flask_login
 from application.models import *
 from datetime import datetime
+from .custom_handles import *
 
 # based on date we get from JavaScript, DO NOT CHANGE
 date_format = '%m/%d/%Y, %I:%M:%S %p'
@@ -69,70 +70,73 @@ class Edit_Log_Form(Add_Log_Form):
 @app.route('/tracker/<int:id>/log/add', methods = ['GET', 'POST'])
 @login_required
 def add_tracker_log(id):
-    tracker_data = Tracker.query.filter_by(user_id=flask_login.current_user.id, id=id).one_or_none()
-    if tracker_data:
-        datatypes = list(set([i.datatype for i in tracker_data.ttype]))
-        data = {
-            'id': tracker_data.id,
-            'name': tracker_data.name,
-            'description': tracker_data.description,
-            'user_id': tracker_data.user_id,
-            'settings': ",".join([i.value for i in tracker_data.settings]),
-            'type': datatypes[0] if len(datatypes) > 0 else '',
-            'choices': {i.id: (i.value if i.value else '') for i in tracker_data.ttype}
-        }
+    try:
+        tracker_data = Tracker.query.filter_by(user_id=flask_login.current_user.id, id=id).one_or_none()
+        if tracker_data:
+            datatypes = list(set([i.datatype for i in tracker_data.ttype]))
+            data = {
+                'id': tracker_data.id,
+                'name': tracker_data.name,
+                'description': tracker_data.description,
+                'user_id': tracker_data.user_id,
+                'settings': ",".join([i.value for i in tracker_data.settings]),
+                'type': datatypes[0] if len(datatypes) > 0 else '',
+                'choices': {i.id: (i.value if i.value else '') for i in tracker_data.ttype}
+            }
 
-        if request.method == 'GET':
-            return render_template('tracker/log.html', tracker=data, title='Log Tracker')
-        else:
-            add_form = Add_Log_Form()
-            if not add_form.validate_on_submit():
-                time_error = False
-                for field in add_form.errors.keys():
-                    if field == 'ldate':
-                        add_form['ldate'].data = datetime.now()
-                        time_error = True
-
-                flash('Validation error occurred while logging tracker', 'error')                
-                return render_template('tracker/log.html', title='Log Tracker', form=add_form, retry=True, tracker=data, date_format=date_format, time_error=time_error)
-
-            if request.form['tid'] == str(id):
-                try:
-                    try:
-                        datetime.strptime(request.form['ldate'], date_format)
-                    except ValueError:
-                        flash(f'Timestamp should be in the format {datetime.strftime(datetime.now(), date_format)}', 'error')
-                        return render_template('tracker/log.html', title='Log Tracker', form=add_form, retry=True, tracker=data, date_format=date_format)
-
-                    log = Tracker_log(tracker_id = tracker_data.id, note = request.form['lnote'], timestamp = datetime.strptime(request.form['ldate'], '%m/%d/%Y, %I:%M:%S %p'))
-                    db.session.add(log)
-                    db.session.flush()
-
-                    if data['type'] == 'ms':
-                        choices = request.form.getlist('lchoice')
-                        for i in choices:
-                            x = Tracker_log_value(log_id = log.id, value = i)
-                            db.session.add(x)                    
-                        db.session.commit()
-                    
-                    else:
-                        x = Tracker_log_value(log_id = log.id, value = int(request.form['lvalue']) if data['type'] == 'integer' else (float(request.form['lvalue']) if data['type'] == 'float' else str(request.form['lvalue'])))
-                        db.session.add(x)
-                        db.session.commit()
-                except:
-                    app.logger.exception(f'Error ocurred while adding tracker log value')
-                    # if any internal error occurs, rollback the database
-                    db.session.rollback()
-                    flash('Internal error occurred, wasn\'t able to add tracker log value', 'error')
-                    return redirect(url_for('add_tracker_log', id=id))
-
-                flash('Succesfully Saved tracker log', 'success')
-                return redirect(url_for('show_tracker_log', id=id))
+            if request.method == 'GET':
+                return render_template('tracker/log.html', tracker=data, title='Log Tracker')
             else:
-                abort(404)
-    
-    else:
-        abort(404)
+                add_form = Add_Log_Form()
+                if not add_form.validate_on_submit():
+                    time_error = False
+                    for field in add_form.errors.keys():
+                        if field == 'ldate':
+                            add_form['ldate'].data = datetime.now()
+                            time_error = True
+
+                    flash('Validation error occurred while logging tracker', 'error')                
+                    return render_template('tracker/log.html', title='Log Tracker', form=add_form, retry=True, tracker=data, date_format=date_format, time_error=time_error)
+
+                if request.form['tid'] == str(id):
+                    try:
+                        try:
+                            datetime.strptime(request.form['ldate'], date_format)
+                        except ValueError:
+                            flash(f'Timestamp should be in the format {datetime.strftime(datetime.now(), date_format)}', 'error')
+                            return render_template('tracker/log.html', title='Log Tracker', form=add_form, retry=True, tracker=data, date_format=date_format)
+
+                        log = Tracker_log(tracker_id = tracker_data.id, note = request.form['lnote'], timestamp = datetime.strptime(request.form['ldate'], '%m/%d/%Y, %I:%M:%S %p'))
+                        db.session.add(log)
+                        db.session.flush()
+
+                        if data['type'] == 'ms':
+                            choices = request.form.getlist('lchoice')
+                            for i in choices:
+                                x = Tracker_log_value(log_id = log.id, value = i)
+                                db.session.add(x)                    
+                            db.session.commit()
+                        
+                        else:
+                            x = Tracker_log_value(log_id = log.id, value = int(request.form['lvalue']) if data['type'] == 'integer' else (float(request.form['lvalue']) if data['type'] == 'float' else str(request.form['lvalue'])))
+                            db.session.add(x)
+                            db.session.commit()
+                    except:
+                        app.logger.exception(f'Error ocurred while adding tracker log value')
+                        # if any internal error occurs, rollback the database
+                        db.session.rollback()
+                        flash('Internal error occurred, wasn\'t able to add tracker log value', 'error')
+                        return redirect(url_for('add_tracker_log', id=id))
+
+                    flash('Succesfully Saved tracker log', 'success')
+                    return redirect(url_for('show_tracker_log', id=id))
+                else:
+                    return show_normal_404()
+        else:
+            return show_normal_404()
+    except:
+        app.logger.exception("error occurred")
+        abort(500)
 
 # =========================================================================================================================
 
@@ -142,71 +146,75 @@ def add_tracker_log(id):
 @app.route('/tracker/<int:tracker_id>/log/<int:log_id>/edit', methods = ['GET', 'POST'])
 @login_required
 def edit_tracker_log(tracker_id, log_id):
-    tracker_data = Tracker.query.filter_by(user_id=flask_login.current_user.id, id=tracker_id).one_or_none()
-    if tracker_data:
-        log_data = Tracker_log.query.filter_by(tracker_id=tracker_data.id, id=log_id).one_or_none()
-        if log_data:
-            datatypes = list(set([i.datatype for i in tracker_data.ttype]))
-            tdata = {
-                'id': tracker_data.id,
-                'name': tracker_data.name,
-                'description': tracker_data.description,
-                'user_id': tracker_data.user_id,
-                'settings': ",".join([i.value for i in tracker_data.settings]),
-                'type': datatypes[0] if len(datatypes) > 0 else '',
-                'choices': {i.id: (i.value.strip() if i.value else '') for i in tracker_data.ttype}
-            }
+    try:
+        tracker_data = Tracker.query.filter_by(user_id=flask_login.current_user.id, id=tracker_id).one_or_none()
+        if tracker_data:
+            log_data = Tracker_log.query.filter_by(tracker_id=tracker_data.id, id=log_id).one_or_none()
+            if log_data:
+                datatypes = list(set([i.datatype for i in tracker_data.ttype]))
+                tdata = {
+                    'id': tracker_data.id,
+                    'name': tracker_data.name,
+                    'description': tracker_data.description,
+                    'user_id': tracker_data.user_id,
+                    'settings': ",".join([i.value for i in tracker_data.settings]),
+                    'type': datatypes[0] if len(datatypes) > 0 else '',
+                    'choices': {i.id: (i.value.strip() if i.value else '') for i in tracker_data.ttype}
+                }
 
-            ldata = {
-                'id': log_data.id,
-                'timestamp': datetime.strftime(log_data.timestamp, date_format),
-                'note': log_data.note,
-                'value': [i.value for i in log_data.values]
-            }
+                ldata = {
+                    'id': log_data.id,
+                    'timestamp': datetime.strftime(log_data.timestamp, date_format),
+                    'note': log_data.note,
+                    'value': [i.value for i in log_data.values]
+                }
 
-            if request.method == 'GET':
-                return render_template('tracker/log.html', title='Edit Log', edit_mode = True, tracker = tdata, log = ldata)
-            
-            else:
-                edit_form = Edit_Log_Form()
-                if not edit_form.validate_on_submit():
-                    flash('Validation error occurred while editing tracker log', 'error')
-                    return render_template('tracker/log.html', title='Edit Log', edit_mode=True, form=edit_form, retry=True, tracker=tdata, log=ldata, date_format=date_format)
+                if request.method == 'GET':
+                    return render_template('tracker/log.html', title='Edit Log', edit_mode = True, tracker = tdata, log = ldata)
                 
-                if request.form['tid'] == str(tracker_data.id) and request.form['lid'] == str(log_data.id):
-                    try:
-                        log_data.timestamp = datetime.strptime(request.form['ldate'], '%m/%d/%Y, %I:%M:%S %p')
-                        log_data.note = request.form['lnote']
-                        
-                        for i in log_data.values:
-                            db.session.delete(i)
-
-                        if tdata['type'] == 'ms':
-                            choices = request.form.getlist('lchoice')
-                            for i in choices:
-                                x = Tracker_log_value(log_id = log_data.id, value = i)
-                                db.session.add(x)
-                        
-                        else:
-                            x = Tracker_log_value(log_id = log_data.id, value = request.form['lvalue'])
-                            db.session.add(x)
-                        
-                        db.session.commit()
-                    except:
-                        app.logger.exception(f'Error ocurred while editing tracker log with id {log_id}')
-                        # if any internal error occurs, rollback the database
-                        db.session.rollback()
-                        flash('Internal error occurred, wasn\'t able to update tracker log value', 'error')
-                        return redirect(url_for('show_tracker_log', id=tracker_id))
-                    
-                    flash('Succesfully updated tracker log', 'success')
-                    return redirect(url_for('show_tracker_log', id=tracker_id))
                 else:
-                    abort(404)
+                    edit_form = Edit_Log_Form()
+                    if not edit_form.validate_on_submit():
+                        flash('Validation error occurred while editing tracker log', 'error')
+                        return render_template('tracker/log.html', title='Edit Log', edit_mode=True, form=edit_form, retry=True, tracker=tdata, log=ldata, date_format=date_format)
+                    
+                    if request.form['tid'] == str(tracker_data.id) and request.form['lid'] == str(log_data.id):
+                        try:
+                            log_data.timestamp = datetime.strptime(request.form['ldate'], '%m/%d/%Y, %I:%M:%S %p')
+                            log_data.note = request.form['lnote']
+                            
+                            for i in log_data.values:
+                                db.session.delete(i)
+
+                            if tdata['type'] == 'ms':
+                                choices = request.form.getlist('lchoice')
+                                for i in choices:
+                                    x = Tracker_log_value(log_id = log_data.id, value = i)
+                                    db.session.add(x)
+                            
+                            else:
+                                x = Tracker_log_value(log_id = log_data.id, value = request.form['lvalue'])
+                                db.session.add(x)
+                            
+                            db.session.commit()
+                        except:
+                            app.logger.exception(f'Error ocurred while editing tracker log with id {log_id}')
+                            # if any internal error occurs, rollback the database
+                            db.session.rollback()
+                            flash('Internal error occurred, wasn\'t able to update tracker log value', 'error')
+                            return redirect(url_for('show_tracker_log', id=tracker_id))
+                        
+                        flash('Succesfully updated tracker log', 'success')
+                        return redirect(url_for('show_tracker_log', id=tracker_id))
+                    else:
+                        return show_normal_404()
+            else:
+                return show_normal_404()
         else:
-            abort(404)
-    else:
-        abort(404)
+            return show_normal_404()
+    except:
+        app.logger.exception("error occurred")
+        abort(500)
 
 # =========================================================================================================================
 
@@ -215,27 +223,31 @@ def edit_tracker_log(tracker_id, log_id):
 @app.route('/tracker/<int:tracker_id>/log/<int:log_id>/delete', methods = ['GET'])
 @login_required
 def delete_tracker_log(tracker_id, log_id):
-    # check if a tracker with the provided id and made by current user exists or not.
-    tracker_data = Tracker.query.filter_by(user_id=flask_login.current_user.id, id=tracker_id).one_or_none()
-    # if it exists, proceed.
-    if tracker_data:
-        log_data = Tracker_log.query.filter_by(tracker_id=tracker_data.id, id=log_id).one_or_none()
-        if log_data:
-            try:            
-                db.session.delete(log_data)
-                db.session.commit()
-            except:
-                app.logger.exception(f'Error ocurred while deleting tracker log with id {log_id}')
-                # if any internal error occurs, rollback the database
-                db.session.rollback()
-                flash('Internal error occurred, wasn\'t able to delete tracker log', 'error')
-                return redirect(url_for('home_page'))
-        
-            flash('Succesfully deleted tracker log', 'success')
-            return redirect(url_for('show_tracker_log', id=tracker_id))
+    try:
+        # check if a tracker with the provided id and made by current user exists or not.
+        tracker_data = Tracker.query.filter_by(user_id=flask_login.current_user.id, id=tracker_id).one_or_none()
+        # if it exists, proceed.
+        if tracker_data:
+            log_data = Tracker_log.query.filter_by(tracker_id=tracker_data.id, id=log_id).one_or_none()
+            if log_data:
+                try:            
+                    db.session.delete(log_data)
+                    db.session.commit()
+                except:
+                    app.logger.exception(f'Error ocurred while deleting tracker log with id {log_id}')
+                    # if any internal error occurs, rollback the database
+                    db.session.rollback()
+                    flash('Internal error occurred, wasn\'t able to delete tracker log', 'error')
+                    return redirect(url_for('home_page'))
+            
+                flash('Succesfully deleted tracker log', 'success')
+                return redirect(url_for('show_tracker_log', id=tracker_id))
+            else:
+                return show_normal_404()
         else:
-            abort(404)
-    else:
-        abort(404)
+            return show_normal_404()
+    except:
+        app.logger.exception("error occurred")
+        abort(500)
 
 # =========================================================================================================================
